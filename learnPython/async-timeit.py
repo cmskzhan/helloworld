6c8a3ad02e93f51ca10784e10fd2af655bbf2167
cmskzhan <homePC-wsl2@nasdaq.com>
2023-01-30 10:46:37 +0000

    coroutine

--- learnPython/async-timeit.py
+++ learnPython/async-timeit.py
@@ -1,6 +1,6 @@
 import time
 import asyncio
-
+import pandas_datareader.data as web
 
 def function_speedo(f):
     async def _fn():
@@ -21,12 +21,24 @@ async def sleep_and_print(seconds):
     print(f"finished async {seconds} sleep ⏰")
     return seconds
 
+async def coroutine_tasks():
+    task = await asyncio.create_task(get_data("AAPL"))
+    await task
+    print(task)
+    return task.result()
+
+async def get_data(symbol:str):
+    return web.DataReader(symbol, "yahoo", "2022-11-17")
+
 
 @function_speedo
 async def main():
     # using arguments
-    results = await asyncio.gather(sleep_and_print(3), sleep_and_print(6))
-    print(results)
+    # results = await asyncio.gather(sleep_and_print(3), sleep_and_print(6))
+    # print(results)
+    AAPL = await coroutine_tasks()
+    print(AAPL)
+
 
 
 asyncio.run(main())
