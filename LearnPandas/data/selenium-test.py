cb4a4e2b1f9772edfb9b40decb89ff4618f9c24d
cmskzhan <homePC-wsl2@nasdaq.com>
2025-08-15 11:55:14 +0100

    a few unconcluded initiative

--- LearnPandas/data/selenium-test.py
+++ LearnPandas/data/selenium-test.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
-
-elem = driver.find_element_by_name("q")
+time.sleep(8)
+elem = driver.find_element(by="name", value="q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 time.sleep(8)
+
 
 driver.close()
